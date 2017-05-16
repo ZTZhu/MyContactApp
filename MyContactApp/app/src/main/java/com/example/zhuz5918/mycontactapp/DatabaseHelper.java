e8c07ef2763c9a268f327743a8f239fbedf44b63
ZTZhu <ZhuZ5918@sduhsd.lan>
2017-05-16 09:29:47 -0700

    Added new fields that DONT WORK!!!!!

--- MyContactApp/app/src/main/java/com/example/zhuz5918/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/zhuz5918/mycontactapp/DatabaseHelper.java
@@ -1,41 +1,55 @@
 package com.example.zhuz5918.mycontactapp;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by zhuz5918 on 5/15/2017.
  */
-    public class DatabaseHelper extends SQLiteOpenHelper{
-        public static final String DATABASE_NAME = "Contact.db";
-        public static final String TABLE_NAME = "contact_table";
-        public static final String COL_1 = "ID";
-        public static final String COL_2 = "NAME";
+    public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(Context context)
-    {
-        super(context, DATABASE_NAME, null, 1);
+    public static final String DATABASE_NAME = "Contact.db";
+    public static final String TABLE_NAME = "contact_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "AGE";
+    public static final String COL_4 = "ADDRESS";
+
+    public DatabaseHelper (Context context){
+        super (context, DATABASE_NAME, null, 1);
     }
 
-    public void onCreate(SQLiteDatabase db)
-    {
-        db.execSQL("CREATE TABLE " + TABLE_NAME +  " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+    @Override
+    public void onCreate (SQLiteDatabase db){
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT, ADDRESS TEXT)");
     }
 
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-    {
+    @Override
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertData(String name)
-    {
+    public boolean insertData(String name, String age, String address){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, name);
-        long result = db.insert(TABLE_NAME, null, contentValues);
-        if(result == -1)return false;
-        else return true;
+        contentValues.put(COL_3, age);
+        contentValues.put(COL_4, address);
 
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1){
+            return false;
+        }
+        return true;
     }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+
 }
--- MyContactApp/app/src/main/java/com/example/zhuz5918/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/zhuz5918/mycontactapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.zhuz5918.mycontactapp;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
 
 
@@ -17,21 +21,90 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         myDb = new DatabaseHelper(this);
+        editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
     }
 
     public void addData(View v){
-        boolean isInserted = myDb.insertData(editName.getText().toString());
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editAddress.getText().toString());
 
         if(isInserted){
             Log.d("MyContact", "Data insertion successful");
             //Create toast message to user indicating data inserted correctly
+            Toast.makeText(v.getContext(), "Success", Toast.LENGTH_LONG).show();
         }
         else{
             Log.d("MyContact", "Data insertion NOT successful");
             //Create toast message to user indicating data inserted correctly
+            Toast.makeText(v.getContext(), "No success", Toast.LENGTH_LONG).show();
         }
     }
+    
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+            Log.d("MyContact", "Error");
+            return;
+        }
+    }
+    StringBuffer buffer = new StringBuffer();
+    //setup loop with moveToNext method
+    //append each COL to buffer
+    //use getString method
+
+    //showMessage("Data", Buffer.toString());
+
+    private void showMessage(String title, String message) {
+    }
+
+
+
+
+
+
+
+    /***
+     *
+     * 
+     *
+     *
+    public void addData(View v){
+     boolean isInserted = myDb.insertData(editName.getText().toString());
+     if(isInserted){
+     Log.d("MyContact", "Name data insertion successful");
+     //Create toast message to user indicating data inserted correctly
+     }
+     else{
+     Log.d("MyContact", "Name data insertion NOT successful");
+     //Create toast message to user indicating data inserted correctly
+     }
+     }
+    public void addAge(View v){
+        boolean isInserted = myDb.insertData(editAge.getText().toString());
+        if(isInserted){
+            Log.d("MyContact", "Age data insertion successful");
+            //Create toast message to user indicating data inserted correctly
+        }
+        else{
+            Log.d("MyContact", "Age data insertion NOT successful");
+            //Create toast message to user indicating data inserted correctly
+        }
+    }
+
+    public void addAddress(View v){
+        boolean isInserted = myDb.insertData(editAddress.getText().toString());
+        if(isInserted){
+            Log.d("MyContact", "Address data insertion successful");
+            //Create toast message to user indicating data inserted correctly
+        }
+        else{
+            Log.d("MyContact", "Address data insertion NOT successful");
+            //Create toast message to user indicating data inserted correctly
+        }
+    }
+    ***/
 
 }
