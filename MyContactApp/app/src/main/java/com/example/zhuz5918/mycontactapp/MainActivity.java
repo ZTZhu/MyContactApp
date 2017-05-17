e2079d84bbb05bca66d4a40d6a5bdb771aec59fe
ZTZhu <ZhuZ5918@sduhsd.lan>
2017-05-17 08:54:40 -0700

    Made Retrieve Data work
    All tasks accomplished except for the very last one of 8.

--- MyContactApp/app/src/main/java/com/example/zhuz5918/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/zhuz5918/mycontactapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zhuz5918.mycontactapp;
 
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+/**
+ * Note to self: Phone #9 is the one that magically makes stuff work
+ */
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
     EditText editName;
@@ -27,30 +32,57 @@ public class MainActivity extends AppCompatActivity {
         editAddress = (EditText) findViewById(R.id.editText_address);
     }
 
-    public void addData(View v){
+    public void addData(View v) {
         boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editAddress.getText().toString());
 
-        if(isInserted){
+        if (isInserted) {
             Log.d("MyContact", "Data insertion successful");
             //Create toast message to user indicating data inserted correctly
             Toast.makeText(v.getContext(), "Success", Toast.LENGTH_LONG).show();
-        }
-        else{
+        } else {
             Log.d("MyContact", "Data insertion NOT successful");
             //Create toast message to user indicating data inserted correctly
             Toast.makeText(v.getContext(), "No success", Toast.LENGTH_LONG).show();
         }
     }
-    
-    public void viewData(View v){
+
+    public void viewData(View v) {
         Cursor res = myDb.getAllData();
-        if(res.getCount() == 0){
+        if (res.getCount() == 0) {
             showMessage("Error", "No data found in database");
             Log.d("MyContact", "Error");
             return;
         }
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+        buffer.append("Name: " + res.getString(1));
+        buffer.append(" /// Address: " + res.getString(2));
+        buffer.append(" /// Age: " + res.getString(3));
+        buffer.append("\n\n");
+        }
+        showMessage("Data", buffer.toString());
+        Log.d("MyContact", buffer.toString());
     }
-    StringBuffer buffer = new StringBuffer();
+
+    public void searchName(View v, String name){
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            Log.d("MyContact", "Error");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            if(res.getString(1).equals(name)){
+                buffer.append("Name: " + res.getString(1));
+                buffer.append("Address: " + res.getString(2));
+                buffer.append("Age: " + res.getString(3));
+            }
+            showMessage("Data", buffer.toString());
+            Log.d("MyContact", buffer.toString());
+        }
+    }
+
     //setup loop with moveToNext method
     //append each COL to buffer
     //use getString method
@@ -58,53 +90,14 @@ public class MainActivity extends AppCompatActivity {
     //showMessage("Data", Buffer.toString());
 
     private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+
     }
 
 
-
-
-
-
-
-    /***
-     *
-     * 
-     *
-     *
-    public void addData(View v){
-     boolean isInserted = myDb.insertData(editName.getText().toString());
-     if(isInserted){
-     Log.d("MyContact", "Name data insertion successful");
-     //Create toast message to user indicating data inserted correctly
-     }
-     else{
-     Log.d("MyContact", "Name data insertion NOT successful");
-     //Create toast message to user indicating data inserted correctly
-     }
-     }
-    public void addAge(View v){
-        boolean isInserted = myDb.insertData(editAge.getText().toString());
-        if(isInserted){
-            Log.d("MyContact", "Age data insertion successful");
-            //Create toast message to user indicating data inserted correctly
-        }
-        else{
-            Log.d("MyContact", "Age data insertion NOT successful");
-            //Create toast message to user indicating data inserted correctly
-        }
-    }
-
-    public void addAddress(View v){
-        boolean isInserted = myDb.insertData(editAddress.getText().toString());
-        if(isInserted){
-            Log.d("MyContact", "Address data insertion successful");
-            //Create toast message to user indicating data inserted correctly
-        }
-        else{
-            Log.d("MyContact", "Address data insertion NOT successful");
-            //Create toast message to user indicating data inserted correctly
-        }
-    }
-    ***/
 
 }
